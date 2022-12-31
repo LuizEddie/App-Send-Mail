017608106907e6cce9a22c3625a2707e85869c85
LuizEddie <contato.luiz1997@gmail.com>
2022-12-31 13:34:23 -0300

    Aula 387

--- processa_envio.php
+++ processa_envio.php
@@ -1,37 +1,84 @@
 <?php
 
-    class Mensagem{
-        private $para = null;
-        private $assunto = null;
-        private $mensagem = null;
+require "./bibliotecas/PHPMailer/Exception.php";
+require "./bibliotecas/PHPMailer/OAuth.php";
+require "./bibliotecas/PHPMailer/PHPMailer.php";
+require "./bibliotecas/PHPMailer/POP3.php";
+require "./bibliotecas/PHPMailer/SMTP.php";
 
-        public function __get($attr){
-            return $this->$attr;
-        }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        public function __set($attr, $value){
-            $this->$attr = $value;
-        }
+class Mensagem
+{
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
 
-        public function mensagemValida(){
-            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)){
-                return false;
-            }
-
-            return true;
-        }
+    public function __get($attr)
+    {
+        return $this->$attr;
     }
 
-    $mensagem = new Mensagem();
-    $mensagem->__set('para', $_POST['para']);
-    $mensagem->__set('assunto', $_POST['assunto']);
-    $mensagem->__set('mensagem', $_POST['mensagem']);
-
-    // print_r($mensagem);
-
-    if($mensagem->mensagemValida()){
-        echo 'Mensagem válida!';
-    }else{
-        echo 'Mensagem inválida, preencha os campos corretamente!';
+    public function __set($attr, $value)
+    {
+        $this->$attr = $value;
     }
-?>
+
+    public function mensagemValida()
+    {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+$mensagem = new Mensagem();
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
+
+// print_r($mensagem);
+
+if (!$mensagem->mensagemValida()) {
+    echo 'Mensagem inválida, preencha os campos corretamente!';
+    die();
+}
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'luiztesteapps2022@gmail.com';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('luiztesteapps2022@gmail.com', 'Web Completo');
+    $mail->addAddress($mensagem->__get('para'));     //Add a recipient
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $mensagem->__get('assunto');
+    $mail->Body    = $mensagem->__get('mensagem');
+    $mail->AltBody = 'É necessário usar um cliente que suporte HTML para ler a mensagem!';
+
+    $mail->send();
+    echo 'Email enviado com sucesso!';
+} catch (Exception $e) {
+    echo "Não foi possível enviar o email, segue detalhes: {$mail->ErrorInfo}";
+}
